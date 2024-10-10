0634d3c1a5cd8d3f00e5db0910868af8bba6ef61
Esmael Ali <154881690+esmizz@users.noreply.github.com>
2024-10-10 16:51:03 +0300

    Update Stores.jsx

--- Src/stores/Stores.jsx
+++ Src/stores/Stores.jsx
@@ -2,7 +2,7 @@
 import { create } from 'zustand';
 
 const useStore = create((set) => ({
-  theme: 'light', // Default theme
+  theme: 'light', // Default state
   toggleTheme: () => set((state) => ({
     theme: state.theme === 'light' ? 'dark' : 'light'
   })),
